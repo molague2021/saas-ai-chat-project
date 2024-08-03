967c41f68783857f88280d45e785fbf8a4f89c77
Martin Olague <molague23@hotmail.com>
2024-08-03 12:48:44 -0600

    Added the PlaceholderDocument component and first client side render component

---
+++ app/dashboard/upload/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const UploadPage = () => {
+  return (
+    <div>
+      {/**File uploader */}
+      UploadPage
+    </div>
+  );
+};
+
+export default UploadPage;
---
+++ components/PlaceholderDocuments.tsx
@@ -0,0 +1,25 @@
+'use-client';
+import { PlusCircleIcon } from 'lucide-react';
+import { Button } from './ui/button';
+import { useRouter } from 'next/router';
+
+const PlaceholderDocuments = () => {
+  const router = useRouter();
+
+  const handleClick = () => {
+    // Check if user is FREE tier and if theyre over the file limit, push tp the upgrade page
+    router.push('/dashboard/upload');
+  };
+
+  return (
+    <Button
+      onClick={handleClick}
+      className="flex flex-col items-center justify-center w-64 h-80 rounded-xl bg-gray-200 drop-shadow-md text-gray-400"
+    >
+      <PlusCircleIcon className="h-16 w-16" />
+      <p>Add a document</p>
+    </Button>
+  );
+};
+
+export default PlaceholderDocuments;
