9a50cc669c4e7c7cb7f99f9c4c6b9db4628dba95
Martin Olague <molague23@hotmail.com>
2024-08-07 08:29:58 -0600

    Added icons and state for when file is being uploaded

--- components/FileUploader.tsx
+++ components/FileUploader.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useCallback } from 'react';
+import { useCallback, useEffect } from 'react';
 import { useDropzone } from 'react-dropzone';
 import {
   CheckCircleIcon,
@@ -9,10 +9,19 @@ import {
   RocketIcon,
   SaveIcon,
 } from 'lucide-react';
-import useUpload from '../hooks/useUpload';
+import useUpload, { StatusText } from '../hooks/useUpload';
+import { useRouter } from 'next/navigation';
 
 const FileUploader = () => {
   const { progress, status, fileId, handleUpload } = useUpload();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (fileId) {
+      router.push(`/dashboard/files/${fileId}`);
+    }
+  }, [fileId, router]);
+
   const onDrop = useCallback(async (acceptedFiles: File[]) => {
     // Do something with the files
     console.log(acceptedFiles);
@@ -22,6 +31,22 @@ const FileUploader = () => {
     } else {
     }
   }, []);
+
+  const statusIcons: {
+    [key in StatusText]: JSX.Element;
+  } = {
+    [StatusText.UPLOADING]: (
+      <RocketIcon className="h-20 w-20 text-indigo-600" />
+    ),
+    [StatusText.UPLOADED]: (
+      <CheckCircleIcon className="h-20 w-20 text-indigo-600" />
+    ),
+    [StatusText.SAVING]: <SaveIcon className="h-20 w-20 text-indigo-600" />,
+    [StatusText.GENERATING]: (
+      <HammerIcon className="h-20 w-20 text-indigo-600" />
+    ),
+  };
+
   const { getRootProps, getInputProps, isDragActive, isFocused, isDragAccept } =
     useDropzone({
       onDrop,
@@ -29,30 +54,61 @@ const FileUploader = () => {
       accept: { 'application/pdf': ['.pdf'] },
     });
 
+  const uploadInProgress =
+    progress !== null && progress >= 0 && progress <= 100;
+
   return (
     <div className="flex flex-col gap-4 items-center max-w-7xl mx-auto">
       {/**Loading... Tomorrow */}
-      <div
-        {...getRootProps()}
-        className={`p-10 border-2 border-dashed mt-10 w-[90%] justify-center rounded-lg h-96 flex items-center border-indigo-600 text-indigo-600 ${
-          isFocused || isDragAccept ? 'bg-indigo-300' : 'bg-indigo-100'
-        }`}
-      >
-        <input {...getInputProps()} />
-        <div className="flex flex-col items-center justify-center">
-          {isDragActive ? (
-            <>
-              <RocketIcon className="h-20 w-20 animate-ping" />
-              <p>Drop the files here ...</p>
-            </>
-          ) : (
-            <>
-              <CircleArrowDown className="h-20 w-20 animate-bounce" />
-              <p>Drag n drop some files here, or click to select files</p>
-            </>
-          )}
+      {uploadInProgress && (
+        <div className="mt-32 flex flex-col justify-center items-center gap-5">
+          <div
+            className={`radial-progress bg-indigo-300 text-white border-indigo-600 border-4 ${
+              progress === 100 && 'hidden'
+            }`}
+            role="progressbar"
+            style={{
+              // @ts-ignore
+              '--value': progress,
+              size: '12rem',
+              '--thickness': '1.3rem',
+            }}
+          >
+            {progress} %
+          </div>
+
+          {/** Render status Icon */}
+          {
+            // @ts-ignore
+            statusIcons[status]
+          }
+          {/* @ts-ignore */}
+          <p className="text-indigo-600 animate-pulse">{status}</p>
         </div>
-      </div>
+      )}
+      {!uploadInProgress && (
+        <div
+          {...getRootProps()}
+          className={`p-10 border-2 border-dashed mt-10 w-[90%] justify-center rounded-lg h-96 flex items-center border-indigo-600 text-indigo-600 ${
+            isFocused || isDragAccept ? 'bg-indigo-300' : 'bg-indigo-100'
+          }`}
+        >
+          <input {...getInputProps()} />
+          <div className="flex flex-col items-center justify-center">
+            {isDragActive ? (
+              <>
+                <RocketIcon className="h-20 w-20 animate-ping" />
+                <p>Drop the files here ...</p>
+              </>
+            ) : (
+              <>
+                <CircleArrowDown className="h-20 w-20 animate-bounce" />
+                <p>Drag n drop some files here, or click to select files</p>
+              </>
+            )}
+          </div>
+        </div>
+      )}
     </div>
   );
 };
