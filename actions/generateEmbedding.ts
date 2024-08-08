231f206002aed544bc317ad0bf5936460569de9c
Martin Olague <molague23@hotmail.com>
2024-08-08 08:38:22 -0600

    Implemented Pinecone to store AI embedding

---
+++ actions/generateEmbedding.ts
@@ -0,0 +1,14 @@
+'use server';
+import { auth } from '@clerk/nextjs/server';
+import { revalidatePath } from 'next/cache';
+
+export const generateEmbedding = async (docId: string) => {
+  auth().protect(); // Protect this route with clark
+
+  // Turn PDF into embeddings 0.012345, 2.20393 etc.
+  await generateEmbeddingsInPineconeVectorStore(docId);
+
+  revalidatePath('/dashboard');
+
+  return { completed: true };
+};
---
+++ app/dashboard/files/[id]/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const ChatToFilePage = ({ params: { id } }: { params: { id: string } }) => {
+  return <div>ChatToFilePage: {id}</div>;
+};
+
+export default ChatToFilePage;
