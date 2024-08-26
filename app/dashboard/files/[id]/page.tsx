5d1b940e3aa6417b7987b75058e603614210f7c6
Martin Olague <molague23@hotmail.com>
2024-08-26 08:58:38 -0600

    Configuring CORS and added PDF view functionality

--- app/dashboard/files/[id]/page.tsx
+++ app/dashboard/files/[id]/page.tsx
@@ -1,7 +1,37 @@
-import React from 'react';
+import PdfView from '../../../../components/PdfView';
+import { adminDb } from '../../../../firebaseAdmin';
+import { auth } from '@clerk/nextjs/server';
 
-const ChatToFilePage = ({ params: { id } }: { params: { id: string } }) => {
-  return <div>ChatToFilePage: {id}</div>;
+const ChatToFilePage = async ({
+  params: { id },
+}: {
+  params: { id: string };
+}) => {
+  auth().protect();
+  const { userId } = await auth();
+
+  const firebaseRef = await adminDb
+    .collection('users')
+    .doc(userId!)
+    .collection('files')
+    .doc(id)
+    .get();
+
+  const url = firebaseRef.data()?.downloadUrl;
+
+  return (
+    <div className="grid lg:grid-cols-5 h-full overflow-hidden">
+      {/** Right Section */}
+      <div className="col-span-5 lg:col-span-2 overflow-y-auto">
+        {/** Chat */}
+      </div>
+      {/** Left Section */}
+      <div className="col-span-5 lg:col-span-3 bg-gray-100 border-r-2 lg:border-indigo-600 lg:-order-1 overflow-auto">
+        {/** PDFView */}
+        <PdfView url={url} />
+      </div>
+    </div>
+  );
 };
 
 export default ChatToFilePage;
